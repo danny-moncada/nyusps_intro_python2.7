d4d849ecbc5d026beffa6df02ec470d651b40fc2
Danny Moncada <32108026+danny-moncada@users.noreply.github.com>
2017-09-19 15:23:25 -0400

    Add files via upload

---
+++ exercise1_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+int_x = 7
+
+int_y = 9
+
+x_times_y = int_x * int_y
+
+print x_times_y
+print type(x_times_y)
---
+++ exercise1_5.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+var = raw_input("Please enter an integer: ")
+
+var2 = int(var) * 2
+
+print str(var) + " doubled is " + str(var2) + "." 
